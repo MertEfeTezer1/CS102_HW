591c7229e31346916745a234e4e565d80078ca73
Doruk Akoğlu <dorukakoglu@gmail.com>
2026-01-28 21:41:45 +0300

    average calculation and differences from average

--- assignment2.java
+++ assignment2.java
@@ -27,4 +27,16 @@ public class deneme {
             }
         }
     }
+    public static double[] findAverageAndDifference(int[]numbers){
+        int sum = 0;
+        for (int number : numbers){
+            sum = sum + number;
+        }
+        double average = (double) (sum / numbers.length);
+        double[] differenceNumbers = new double[numbers.length];
+        for(int i = 0;i<numbers.length;i++){
+            differenceNumbers[i] = numbers[i] - average;
+        }
+        return differenceNumbers;
     }
+}
