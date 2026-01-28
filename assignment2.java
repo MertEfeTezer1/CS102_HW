958c1f661cc822f2b70e399b36b3fe80a160a9b7
Mert Efe Tezer <tezermertefe@gmail.com>
2026-01-28 21:10:28 +0300

    initialize array and take input

--- assignment2.java
+++ assignment2.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class deneme {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number for cap the array");
+        int cap = input.nextInt();
+        input.close();
+        int [] numbers = new int[cap];
+        
+    }
+    }
