5f7322b475ab42fa2b82fe76758182d34ba29500
Berat Çalı <beratcali40@gmail.com>
2026-01-28 21:18:44 +0300

    Display the menu options and take a choice from the user.

--- assignment2.java
+++ assignment2.java
@@ -5,8 +5,26 @@ public class deneme {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number for cap the array");
         int cap = input.nextInt();
-        input.close();
         int [] numbers = new int[cap];
-        
+        boolean check = false;
+        System.out.print("Enter your choice 1-4. 5 for the exit.");
+        int choice = in.nextInt;
+        while(!check){
+            if(choice = 1){
+                findMin(numbers);
+            }
+            else if(choice = 2){
+                findMax(numbers);
+            }
+            else if(choice = 3){
+                findAverageAndDifference(numbers);
+            }
+            else if(choice = 4){
+                findSum(numbers);
+            }
+            else if(choice = 5){
+                check = true;
+            }
+        }
     }
     }
